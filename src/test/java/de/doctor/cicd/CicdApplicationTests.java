b4b1c78e087eb33bd90f6f45d5202996ad5759f0
c.altin <c.altin@outlook.com>
2024-02-22 14:25:05 +0100

    init

---
+++ src/test/java/de/doctor/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package de.doctor.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
